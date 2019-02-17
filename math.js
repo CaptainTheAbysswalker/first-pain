2ce58d3e606c3de548e2117ec877b071431c24c2
CaptainUragan <shuric03@gmail.com>
2019-02-17 22:26:03 +0300

    init commit

---
+++ math.js
@@ -0,0 +1,17 @@
+
+window.onload = function(){
+var pointer = document.getElementById("point");
+
+function getXPositionOfElement() {
+    var x_position = Math.floor(Math.random() * window.innerWidth);
+    pointer.style.top = x_position;
+}
+function getYPositionOfElement() {
+    var y_position = Math.floor(Math.random() * window.innerHeight);
+    pointer.style.left = y_position;
+}
+
+    getXPositionOfElement();
+    getYPositionOfElement();
+
+}
