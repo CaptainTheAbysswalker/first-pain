e34e1ab31a3a67de7f3a349d9d6b03e44c8f8b4b
CaptainUragan <shuric03@gmail.com>
2019-02-18 20:12:55 +0300

    fixed point in circle

--- math.js
+++ math.js
@@ -1,14 +1,16 @@
 
-// var x_max = +document.getElementById("square").style.height;
-// var y_max = +document.getElementById("square").style.width;
+var CIRCLE_SHIFT = 10;
 var pointer = document.getElementById("point");
 var circler = document.getElementById("circle");
-var pos_x = +circler.style.left.replace('px','');
-var pos_y = +circler.style.left.replace('px','');
-var square_width = +document.getElementById('square').style.width.replace('px','');
-var square_height = +document.getElementById('square').style.height.replace('px','');
-var circle_width = +document.getElementById('circle').style.width.replace('px','');
-var circle_height = +document.getElementById('circle').style.height.replace('px','');
+var pos_x = parseInt(circler.style.left.replace('px',''), 10);
+var pos_y = parseInt(circler.style.left.replace('px',''), 10);
+var square_width = parseInt(document.getElementById('square').style.width.replace('px',''), 10);
+var square_height = parseInt(document.getElementById('square').style.height.replace('px',''), 10);
+var circle_width = parseInt(document.getElementById('circle').style.width.replace('px',''), 10);
+var circle_height = parseInt(document.getElementById('circle').style.height.replace('px',''), 10);
+var rad = parseInt(document.getElementById('circle').style.width.replace('px',''), 10)/2;
+
+
 
 function getXPositionOfPoint() {
     var x_position = Math.floor(Math.random() * square_width);
@@ -33,25 +35,27 @@ function moveCircle(keyCode){
     var pos_y = +circler.style.top.replace('px','');
     switch (keyCode){
         case 37: 
-            if (pos_x != 0 ) circler.style.left = (pos_x - 10) + 'px';
+            if (pos_x != 0 ) circler.style.left = (pos_x - CIRCLE_SHIFT) + 'px';
             break;
         case 38:
-            if (pos_y != 0)circler.style.top = (pos_y - 10) + 'px';
+            if (pos_y != 0)circler.style.top = (pos_y - CIRCLE_SHIFT) + 'px';
             break;
         case 39:
-            if (pos_x < (square_width - circle_width)) circler.style.left = (pos_x + 10) + 'px';
+            if (pos_x + CIRCLE_SHIFT < (square_width - circle_width)) circler.style.left = (pos_x + 10) + 'px';
             break;
         case 40:
-            if (pos_y  < (square_height - circle_height))circler.style.top = (pos_y + 10) + 'px';
+            if (pos_y + CIRCLE_SHIFT < (square_height - circle_height))circler.style.top = (pos_y + 10) + 'px';
             break;
     }
-    x_position = +pointer.style.left.replace('px','');
-    y_position = +pointer.style.top.replace('px','');
-    pos_x = +circler.style.left.replace('px','');
-    pos_y = +circler.style.top.replace('px','');
-    if((x_position >= pos_x && x_position <= pos_x + circle_width) && 
-    (y_position >= pos_y && y_position <= pos_y + circle_height)){
-        
+    var midXCoord = parseInt(circler.style.left.replace('px',''), 10) +rad;
+    var midYCoord = parseInt(circler.style.top.replace('px',''), 10) +rad;
+    console.log(midXCoord);
+    var xCoordByMid = midXCoord - parseInt(pointer.style.left.replace('px',''), 10);
+    var yCoordByMid = midYCoord - parseInt(pointer.style.top.replace('px',''), 10);
+
+    pos_x = parseInt(circler.style.left.replace('px',''), 10);
+    pos_y = parseInt(circler.style.top.replace('px',''), 10);
+    if( xCoordByMid * xCoordByMid + yCoordByMid * yCoordByMid < rad * rad){
        getNewPoint();
        setTimeout("alert('Good Job!!!')", 1);
     }
